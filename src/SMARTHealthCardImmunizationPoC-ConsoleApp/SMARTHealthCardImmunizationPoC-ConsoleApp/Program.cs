d831e43504ba58d20052f7f40c22f312b465ce7e
snowke <6555327+snowke@users.noreply.github.com>
2021-08-27 14:01:45 -0600

    changed iss to lowercase + cleaned up for review

--- src/SMARTHealthCardImmunizationPoC-ConsoleApp/SMARTHealthCardImmunizationPoC-ConsoleApp/Program.cs
+++ src/SMARTHealthCardImmunizationPoC-ConsoleApp/SMARTHealthCardImmunizationPoC-ConsoleApp/Program.cs
@@ -1,6 +1,7 @@
-﻿using SmartHealthCard.QRCode;
+﻿using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
+using SmartHealthCard.QRCode;
 using SmartHealthCard.Token;
-using SmartHealthCard.Token.Certificates;
 using SmartHealthCard.Token.Exceptions;
 using SmartHealthCard.Token.Model.Shc;
 using System;
@@ -9,12 +10,14 @@ using System.Drawing;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 
-using Hl7.Fhir.Model;
-using Hl7.Fhir.Serialization;
-
 namespace SMARTHealthCardImmunizationPoC_ConsoleApp
 {
     /// <summary>
+    ///
+    /// Library for generating SMART Health Cards and used for this example
+    ///     https://github.com/angusmillar/SmartHealthCard
+    ///     https://www.nuget.org/packages/SmartHealthCard.QRCode/
+    ///     
     /// FHIR Client Library Documentation
     ///     https://docs.fire.ly/projects/Firely-NET-SDK/
     /// 
@@ -23,10 +26,6 @@ namespace SMARTHealthCardImmunizationPoC_ConsoleApp
     /// 
     /// FHIR Immunization Resource
     ///     https://www.hl7.org/fhir/immunization.html
-    ///     
-    /// Library for generating SMART Health Cards
-    ///     https://github.com/angusmillar/SmartHealthCard
-    ///     https://www.nuget.org/packages/SmartHealthCard.QRCode/
     ///     
     /// Nice article about decoding
     ///     https://www.eff.org/deeplinks/2021/06/decoding-californias-new-digital-vaccine-records-and-potential-dangers
@@ -50,6 +49,10 @@ namespace SMARTHealthCardImmunizationPoC_ConsoleApp
     ///     openssl pkcs12 -export -inkey imm-poc-private-key.pem -in imm-poc-self-signed-cert.pem -out imm-poc-self-signed-cert.pfx
     /// 
     /// </summary>
+    /// <remarks>
+    ///     For testing purposes we are creating a value near identical to one of the published examples
+    ///     https://spec.smarthealth.cards/examples/example-00-b-jws-payload-expanded.json
+    /// </remarks>
     class Program
     {
         static void Main(string[] args)
@@ -58,19 +61,95 @@ namespace SMARTHealthCardImmunizationPoC_ConsoleApp
             EncoderDemoRunner().Wait();
         }
 
-        private static Patient GetPatientResource()
+        static async System.Threading.Tasks.Task EncoderDemoRunner()
+        {
+            //Get the Certificate containing a private Elliptic Curve key using the P-256 curve
+            //from the Windows Certificate Store by Thumb-print
+            string CertificateThumbprint = "4cdfd4d8b070a894bc701a49cab2ba18f724c2c7";
+
+            var Certificate = GetCertificate(CertificateThumbprint, StoreName.My, StoreLocation.CurrentUser);
+
+            //Set the Version of FHIR in use
+            string FhirVersion = "4.0.1";
+
+            var FhirBundleJson = await GetTestFhirBundle();
+
+            //Set the base of the URL where any validator will retrieve the public keys from (e.g : [Issuer]/.well-known/jwks.json) 
+            //Uri Issuer = new Uri("https://testing.envisiontechnology.com/HL7SmartHealthCardDemo");
+            // Try all lowercase
+            Uri Issuer = new Uri("https://testing.envisiontechnology.com/hl7Smarthealthcarddemo");
+
+            //Set when the Smart Health Card becomes valid, (e.g the from date).
+            DateTimeOffset IssuanceDateTimeOffset = DateTimeOffset.Now.AddMinutes(-1);
+
+            //Set the appropriate VerifiableCredentialsType enum list, for more info see: see: https://smarthealth.cards/vocabulary/
+            List<VerifiableCredentialType> VerifiableCredentialTypeList = new List<VerifiableCredentialType>()
+              {
+                VerifiableCredentialType.HealthCard,
+                VerifiableCredentialType.Immunization,
+                VerifiableCredentialType.Covid19
+              };
+
+            //Instantiate and populate the Smart Health Card Model with the properties we just setup
+            SmartHealthCardModel SmartHealthCard = new SmartHealthCardModel(Issuer, IssuanceDateTimeOffset,
+                new VerifiableCredential(VerifiableCredentialTypeList,
+                  new CredentialSubject(FhirVersion, FhirBundleJson)));
+
+            //Instantiate the Smart Health Card Encoder
+            SmartHealthCardEncoder SmartHealthCardEncoder = new SmartHealthCardEncoder();
+
+            string SmartHealthCardJwsToken = string.Empty;
+            try
+            {
+                //Get the Smart Health Card JWS Token 
+                SmartHealthCardJwsToken = await SmartHealthCardEncoder.GetTokenAsync(Certificate, SmartHealthCard);
+            }
+            catch (SmartHealthCardEncoderException EncoderException)
+            {
+                Console.WriteLine("The SMART Health Card Encoder has found an error, please see message below:");
+                Console.WriteLine(EncoderException.Message);
+            }
+            catch (Exception Exception)
+            {
+                Console.WriteLine("Oops, there is an unexpected development exception");
+                Console.WriteLine(Exception.Message);
+            }
+
+            //Instantiate the Smart Health Card QR Code Factory
+            SmartHealthCardQRCodeEncoder SmartHealthCardQRCodeEncoder = new SmartHealthCardQRCodeEncoder();
+
+            // Test Code
+            List<string> QRCodeRawDataList = SmartHealthCardQRCodeEncoder.GetQRCodeRawDataList(SmartHealthCardJwsToken);
+            SmartHealthCardQRCodeDecoder SmartHealthCardQRCodeDecoder = new SmartHealthCardQRCodeDecoder();
+            string JWS = SmartHealthCardQRCodeDecoder.GetToken(QRCodeRawDataList);
+
+            //Get list of SMART Health Card QR Codes images
+            //Note: If the SMART Health Card JWS payload is large then it will be split up into multiple QR Code images.
+            //SMART Health Card QR Code scanners can scan each image in any order to obtain the whole SMART Health Card  
+            List<Bitmap> QRCodeImageList = SmartHealthCardQRCodeEncoder.GetQRCodeList(SmartHealthCardJwsToken);
+
+            //Write to file the SMART Health Card QR Codes images      
+            for (int i = 0; i < QRCodeImageList.Count; i++)
+            {
+                QRCodeImageList[i].Save(@$"C:\Temp\SMARTHealthCard\QRCode-{i}.png", System.Drawing.Imaging.ImageFormat.Png);
+            }
+
+            return;
+        }
+
+        private static Patient GetPatientResource(string firstName, string middleName, string lastName, Date birthDate)
         {
             return new Patient()
             {
-                BirthDate = "1999-01-01",
+                BirthDateElement = birthDate,
                 Name = new List<HumanName>()
                 {
-                    new HumanName().WithGiven("Bart").WithGiven("E.").AndFamily("Simpson")
+                    new HumanName().WithGiven(firstName).WithGiven(middleName).AndFamily(lastName)
                 }
             };
         }
 
-        private static Immunization GetImmunizationResource(string referencePrefix, int index, string cvx, FhirDateTime administrationDate)
+        private static Immunization GetImmunizationResource(string referencePrefix, string cvx, FhirDateTime administrationDate, string lotNumber)
         {
             var codeSystemCvx = "http://hl7.org/fhir/sid/cvx";
 
@@ -80,35 +159,43 @@ namespace SMARTHealthCardImmunizationPoC_ConsoleApp
                 Occurrence = (DataType)administrationDate, 
                 Patient = new ResourceReference($"{referencePrefix}:0"),
                 Status = Immunization.ImmunizationStatusCodes.Completed,
-                VaccineCode = new CodeableConcept(codeSystemCvx, cvx) // description is optional and we want to keep size down
+                VaccineCode = new CodeableConcept(codeSystemCvx, cvx), // description is optional and we want to keep size down
+                LotNumber = lotNumber,
+                Performer = new List<Immunization.PerformerComponent>()
+                {
+                   new Immunization.PerformerComponent()
+                   {
+                       Actor = new ResourceReference()
+                       {
+                           Display = "ABC General Hospital"
+                       }
+                   }
+                }
             };
         }
 
-        static async Task<string> GetTestFhirBundle()
+        private static async Task<string> GetTestFhirBundle()
         {
             var referencePrefix = "resource";
  
-
             var bundle = new Bundle
             {
                 Type = Bundle.BundleType.Collection
             };
 
-            var patientSubjectReference = new ResourceReference($"{referencePrefix}:0");
-
             bundle.Entry.Add(
                     new Bundle.EntryComponent()
                     {
                         FullUrl = $"{referencePrefix}:0",
-                        Resource = GetPatientResource()
+                        Resource = GetPatientResource("John", "B.", "Anyperson", new Date(1951, 1, 20))
                     }
-                );
+                ); ;
 
             bundle.Entry.Add(
                     new Bundle.EntryComponent()
                     {
                         FullUrl = $"{referencePrefix}:1",
-                        Resource = GetImmunizationResource(referencePrefix, 1, "207", new FhirDateTime(2021, 7, 1))
+                        Resource = GetImmunizationResource(referencePrefix, "207", new FhirDateTime(2021, 1, 1), "0000001")
                     }
                 );
 
@@ -116,7 +203,7 @@ namespace SMARTHealthCardImmunizationPoC_ConsoleApp
                     new Bundle.EntryComponent()
                     {
                         FullUrl = $"{referencePrefix}:2",
-                        Resource = GetImmunizationResource(referencePrefix, 2, "207", new FhirDateTime(2021, 7, 29))
+                        Resource = GetImmunizationResource(referencePrefix, "207", new FhirDateTime(2021, 1, 29), "0000007")
                     }
                 );
 
@@ -148,85 +235,6 @@ namespace SMARTHealthCardImmunizationPoC_ConsoleApp
             {
                 store.Close();
             }
-
-        }
-
-
-        static async System.Threading.Tasks.Task EncoderDemoRunner()
-        {
-
-            //Get the Certificate containing a private Elliptic Curve key using the P-256 curve
-            //from the Windows Certificate Store by Thumb-print
-            string CertificateThumbprint = "4cdfd4d8b070a894bc701a49cab2ba18f724c2c7";
-
-            var Certificate = GetCertificate(CertificateThumbprint, StoreName.My, StoreLocation.CurrentUser);
-
-            //Set the Version of FHIR in use
-            string FhirVersion = "4.0.1";
-
-            //This library does not validate that the FHIR Bundle provided is valid FHIR, it only parses it as valid JSON.      
-            //I strongly suggest you use the FIRELY .NET SDK as found here: https://docs.fire.ly/projects/Firely-NET-SDK/index.html       
-            //See the FHIR SMART Health Card FHIR profile site here: http://build.fhir.org/ig/dvci/vaccine-credential-ig/branches/main/index.html   
-
-            //Set a FHIR Bundle as a JSON string. 
-            //string FhirBundleJson = "[A Smart Health Card FHIR Bundle in JSON format]";
-            var FhirBundleJson = await GetTestFhirBundle();
-
-            //Set the base of the URL where any validator will retrieve the public keys from (e.g : [Issuer]/.well-known/jwks.json) 
-            Uri Issuer = new Uri("https://acmecare.com/shc");
-
-            //Set when the Smart Health Card becomes valid, (e.g the from date).
-            DateTimeOffset IssuanceDateTimeOffset = DateTimeOffset.Now.AddMinutes(-1);
-
-            //Set the appropriate VerifiableCredentialsType enum list, for more info see: see: https://smarthealth.cards/vocabulary/
-            List<VerifiableCredentialType> VerifiableCredentialTypeList = new List<VerifiableCredentialType>()
-              {
-                VerifiableCredentialType.HealthCard,
-                VerifiableCredentialType.Covid19,
-                VerifiableCredentialType.Immunization
-              };
-
-            //Instantiate and populate the Smart Health Card Model with the properties we just setup
-            SmartHealthCardModel SmartHealthCard = new SmartHealthCardModel(Issuer, IssuanceDateTimeOffset,
-                new VerifiableCredential(VerifiableCredentialTypeList,
-                  new CredentialSubject(FhirVersion, FhirBundleJson)));
-
-            //Instantiate the Smart Health Card Encoder
-            SmartHealthCardEncoder SmartHealthCardEncoder = new SmartHealthCardEncoder();
-
-            string SmartHealthCardJwsToken = string.Empty;
-            try
-            {
-                //Get the Smart Health Card JWS Token 
-                SmartHealthCardJwsToken = await SmartHealthCardEncoder.GetTokenAsync(Certificate, SmartHealthCard);
-            }
-            catch (SmartHealthCardEncoderException EncoderException)
-            {
-                Console.WriteLine("The SMART Health Card Encoder has found an error, please see message below:");
-                Console.WriteLine(EncoderException.Message);
-            }
-            catch (Exception Exception)
-            {
-                Console.WriteLine("Oops, there is an unexpected development exception");
-                Console.WriteLine(Exception.Message);
-            }
-
-            //Instantiate the Smart Health Card QR Code Factory
-            SmartHealthCardQRCodeEncoder SmartHealthCardQRCodeEncoder = new SmartHealthCardQRCodeEncoder();
-
-
-            //Get list of SMART Health Card QR Codes images
-            //Note: If the SMART Health Card JWS payload is large then it will be split up into multiple QR Code images.
-            //SMART Health Card QR Code scanners can scan each image in any order to obtain the whole SMART Health Card  
-            List<Bitmap> QRCodeImageList = SmartHealthCardQRCodeEncoder.GetQRCodeList(SmartHealthCardJwsToken);
-
-            //Write to file the SMART Health Card QR Codes images      
-            for (int i = 0; i < QRCodeImageList.Count; i++)
-            {
-                QRCodeImageList[i].Save(@$"C:\Temp\SMARTHealthCard\QRCode-{i}.png", System.Drawing.Imaging.ImageFormat.Png);
-            }
-
-            return;
         }
     }
 }
